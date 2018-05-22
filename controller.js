5b3a49970aa0c38ed1ad3a67138eac91bd67cc87
hannahsaintonge <hannahsaintonge@gmail.com>
2018-05-21 22:48:11 -0400

    added interactivity to todo list

--- controller.js
+++ controller.js
@@ -19,9 +19,29 @@ function todoController() {
         {
             task: "do laundry.",
             completed: false
-        }
+        }   
     
     ];
+
+    vm.addTask = (task, completed) => {
+        console.log("add button is working");
+        vm.items.push({
+          task: task,
+          completed: false
+        });
+
+      }; 
+
+    vm.removeTask = (index, thing) => {
+        console.log("remove task is working");
+        vm.items.splice(index, 1);  
+    } 
+
+    vm.completeTask = (index) => {
+        vm.items[index].completed = true;
+    }
+
+
 
 }
 
